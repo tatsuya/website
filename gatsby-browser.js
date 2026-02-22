433bd393770212feaf2df1701026505024d76b28
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2026-02-21 23:32:09 -0500

    Replace deprecated typeface-* packages with @fontsource/*

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,9 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+import "@fontsource/montserrat/400.css"
+import "@fontsource/montserrat/700.css"
+import "@fontsource/montserrat/900.css"
+import "@fontsource/merriweather/400.css"
+import "@fontsource/merriweather/700.css"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
